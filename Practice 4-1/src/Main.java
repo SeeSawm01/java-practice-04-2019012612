5e27fbe8783fb4be3e40bed7fd65196d99a2db3c
Lee Min Sung <minsung2556@gmail.com>
2025-03-25 11:28:30 +0900

    HelloWorld 프로젝트 추가

---
+++ Practice 4-1/src/Main.java
@@ -0,0 +1,15 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        //1. (이름은 2글자 이상) 이름 선언(String name = "이민성")
+        //2. (나이는 소수점이 아닌 정수형) 나이 선언(int age = "26")
+        //3. 이름, 나이 출력 System.out.println
+        // print 길게 쓰는 이유 : 클래스이기 때문에
+
+
+        String name = "이민성";
+        int age = 26;
+        System.out.println(name + "님은" + age + "살입니다.");
+        }
+    }
